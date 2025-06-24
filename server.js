41edf9ff8e9a983148970f7066802962ebf2a50b
Deusto <inigo.murga@opendeusto.es>
2025-06-24 21:42:38 +0200

    código final

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let contador = 0;
-let contador2 = 0;
-let contador3 = 0;
-
 let diceHistory = [];
 
 app.post('/roll', (req, res) => {
